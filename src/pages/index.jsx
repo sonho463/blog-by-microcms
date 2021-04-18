171aeee06ca46411a18a093dd3081e46c8fe0300
sonho463 <son.ho463@gmail.com>
2021-04-18 17:57:49 +0900

    googleFonts Applyed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 
-import "../styles/global.css";
+// importの順番大事！！
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles/global.css";
 
 
 export default function Home() {
